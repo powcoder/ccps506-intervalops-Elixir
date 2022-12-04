7d45df1fd5cdcc437a6c86447bf806e9d084f47a
powcoder <powcoder@163.com>
2022-12-04 14:21:42 +0800

    ccps506 intervalops Elixir

---
+++ lib/intervalops.ex
@@ -0,0 +1,45 @@
+defmodule IntervalOps do
+  @moduledoc """
+    Documentation for `IntervalOps`.
+  """
+
+  @doc """
+    Function stubs for you to fill in are given below. Feel free to add as many 
+    additional helper functions as you want. 
+
+    The tests for these functions can be found in test/intervalops_test.exs. 
+    You are encouraged to add your own tests in addition to those provided.
+
+    Run the tester by executing 'mix test' from the intervalops directory 
+    (the one containing mix.exs)
+  """
+
+  def expand(intervals) do
+    []
+  end
+
+  def collapse(items) do
+    ""
+  end
+
+  def union(intervals_1, intervals_2) do
+    ""
+  end
+
+  def intersection(intervals_1, intervals_2) do
+    ""
+  end
+
+  def difference(intervals_1, intervals_2) do
+    ""
+  end
+
+  def is_disjoint(intervals_1, intervals_2) do
+    true
+  end
+
+  def is_subset(intervals_1, intervals_2) do
+    true
+  end
+
+end
